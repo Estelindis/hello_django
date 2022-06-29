7a82cbdda93fdc52d7bd8882f37c6b2bfed42a40
Estelindis <estelindis@gmail.com>
2022-06-29 18:27:00 +0000

    Set up environment variables, update page titles

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--2d_ukodnswd5j6zzxk(qnk!8*v%-_&cbbf5gebm8*jtj&bdu3'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--2d_ukodnswd5j6zzxk(qnk!8*v%-_&cbbf5gebm8*jtj&bdu3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hello-django-to-do.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
